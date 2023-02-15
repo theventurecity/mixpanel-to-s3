26ed56bdda8170c4d54d1451d6fd7d2c01083c57
Ankur Bhatia <ankurbhatia@Ankurs-MacBook-Pro.local>
2023-02-15 20:13:51 +0530

    added glue

---
+++ tf/00-pipeline/glue.tf
@@ -0,0 +1,59 @@
+# this will be inside the ecs folder later.
+
+resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
+  name = "mixpanel"
+}
+
+resource "aws_glue_crawler" "example" {
+  database_name = aws_glue_catalog_database.aws_glue_catalog_database.name
+  name          = "mixpanel-crawler"
+  role          = aws_iam_role.glue.arn
+    table_prefix = "stg_"
+  s3_target {
+    path = "s3://appi-redshift-mixpanel-nonprod/mixpanel/year=2023/"
+  }
+}
+
+resource "aws_iam_role" "glue" {
+  name = "GlueS3RoleMixpanel"
+  tags = local.default_tags
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "glue.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+# resource "aws_iam_policy_attachment" "sqs_glue" {
+#   name       = "SQSForGlue"
+#   roles      = [aws_iam_role.glue.name]
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccesss"
+# }
+
+resource "aws_iam_policy_attachment" "s3_glue" {
+  name       = "S3ForGlue"
+  roles      = [aws_iam_role.glue.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_policy_attachment" "service_glue" {
+  name       = "GlueServiceRole"
+  roles      = [aws_iam_role.glue.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_policy_attachment" "console_glue" {
+  name       = "GlueConsole"
+  roles      = [aws_iam_role.glue.name]
+  policy_arn = "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess"
+}
