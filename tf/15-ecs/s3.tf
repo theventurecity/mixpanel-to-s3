984b23b4dcfe53621b74a7a5c5b584b6fe9f4c48
Basim Hennawi <basim.a.hennawi@gmail.com>
2023-02-28 11:55:29 +0100

    Update s3.tf

--- tf/15-ecs/s3.tf
+++ tf/15-ecs/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "appi_mixpanel_bucket" {
+resource "aws_s3_bucket" "appi_redshift_mixpanel_bucket" {
   bucket = "appi-redshift-mixpanel-${var.env}"
   tags = merge(local.default_tags, {
     VantaDescription      = "bucket for mixpanel data"
@@ -14,18 +14,18 @@ resource "aws_s3_bucket" "appi_mixpanel_bucket" {
 }
 
 resource "aws_s3_bucket_versioning" "appi_mixpanel_bucket_versioning" {
-  bucket = aws_s3_bucket.appi_mixpanel_bucket.id
+  bucket = aws_s3_bucket.appi_redshift_mixpanel_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
-resource "aws_s3_bucket_public_access_block" "appi_media_bucket_block_public_ccess" {
+resource "aws_s3_bucket_public_access_block" "appi_redshift_mixpanel_bucket_block_public_access" {
   bucket                  = "appi-redshift-mixpanel-${var.env}"
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-  depends_on              = [aws_s3_bucket.appi_mixpanel_bucket]
+  depends_on              = [aws_s3_bucket.appi_redshift_mixpanel_bucket]
 }
 
 
@@ -51,7 +51,7 @@ resource "aws_s3_bucket_versioning" "appi_mixpanel_bucket_versioning" {
     status = "Enabled"
   }
 }
-resource "aws_s3_bucket_public_access_block" "appi_mixpanel_bucket_block_public_ccess" {
+resource "aws_s3_bucket_public_access_block" "appi_redshift_mongo_bucket_block_public_access" {
   bucket                  = "appi-redshift-mongo-${var.env}"
   block_public_acls       = true
   block_public_policy     = true
